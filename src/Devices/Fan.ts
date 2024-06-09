fe6d5e2eb381ef2e0fc50b8422c8c74da56b1594
Michael Kellsy <mkellsy@gmail.com>
2024-06-09 09:25:03 -0700

    make lutron conform to baf specs

--- src/Devices/Fan.ts
+++ src/Devices/Fan.ts
@@ -26,7 +26,7 @@ export class Fan extends Common<FanState> implements Interfaces.Fan {
         super(Interfaces.DeviceType.Fan, processor, area, zone, { state: "Off", speed: 0 });
 
         this.fields.set("state", { type: "String", values: ["On", "Off"] });
-        this.fields.set("speed", { type: "Integer", min: 0, max: 4 });
+        this.fields.set("speed", { type: "Integer", min: 0, max: 7 });
     }
 
     /**
@@ -34,18 +34,19 @@ export class Fan extends Common<FanState> implements Interfaces.Fan {
      * state.
      *
      * ```js
-     * fan.update({ SwitchedLevel: "On", FanSpeed: 4 });
+     * fan.update({ SwitchedLevel: "On", FanSpeed: 7 });
      * ```
      *
      * @param status The current device state.
      */
     public update(status: Interfaces.ZoneStatus): void {
         const previous = { ...this.status };
+        const speed = this.parseFanSpeed(status.FanSpeed as unknown as string);
 
         this.state = {
             ...previous,
-            state: status.SwitchedLevel || "Unknown",
-            speed: status.FanSpeed || 0,
+            state: speed > 0 ? "On" : "Off",
+            speed,
         };
 
         if (!equals(this.state, previous)) {
@@ -63,9 +64,52 @@ export class Fan extends Common<FanState> implements Interfaces.Fan {
      * @param status Desired device state.
      */
     public set(status: FanState): Promise<void> {
+        const speed = status.state === "Off" ? 0 : this.lookupFanSpeed(status.speed);
+
         return this.processor.command(this.address, {
             CommandType: "GoToFanSpeed",
-            FanSpeedParameters: [{ FanSpeed: status.state === "Off" ? 0 : status.speed }],
+            FanSpeedParameters: [{ FanSpeed: speed }],
         });
+    }
+
+    private lookupFanSpeed(value: number): string {
+        switch (value) {
+            case 1:
+                return "Low";
+
+            case 2:
+            case 3:
+                return "Medium";
+
+            case 4:
+            case 5:
+                return "MediumHigh";
+
+            case 6:
+            case 7:
+                return "High";
+
+            default:
+                return "Off";
+        }
+    }
+
+    private parseFanSpeed(value: string): number {
+        switch (value) {
+            case "Low":
+                return 1;
+
+            case "Medium":
+                return 3;
+
+            case "MediumHigh":
+                return 5;
+
+            case "High":
+                return 7;
+
+            default:
+                return 0;
+        }
     }
 }
